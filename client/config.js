7301a8ac209ddca1c6bb1cefd95a5e38fcef122a
Faizan Iqbal <faizaniqbal917@gmail.com>
2025-05-30 15:40:12 -0700

    Fix API_URL in config.js to remove redundant /api path in production

--- client/config.js
+++ client/config.js
@@ -3,6 +3,6 @@ const dev = process.env.NODE_ENV !== 'production';
 // We'll deploy the backend to Netlify Functions
 export const API_URL = dev 
   ? 'http://localhost:5000/api' 
-  : '/.netlify/functions/api';
+  : '/.netlify/functions';
 
 export const APP_NAME = 'PersonalWeb';
